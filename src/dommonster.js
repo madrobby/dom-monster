3a4de6182f366c1bb72a6871a7b4f34260c798f2
Thomas Fuchs <thomas@fesch.at>
2011-01-18 12:03:29 +0100

    v1.2.3

--- src/dommonster.js
+++ src/dommonster.js
@@ -6,7 +6,7 @@
  */
 
 (function(){
-  if(!('JR' in window)) window.JR = { Version: '1.2.2' };
+  if(!('JR' in window)) window.JR = { Version: '1.2.3' };
   var JR = window.JR;
 
   function $(id){ return document.getElementById(id); }
